b3fce083bbb65005aaf196ed46f34549459b6972
Eddie Villarreal Home <eddie.villarreal@outlook.com>
2020-08-29 13:41:41 -0500

    added ajax call for spoonacular

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 //toggle switch between restaurant and recipe
 //check box for cuisine parameter, possible an array
 //click func for search
+
+
+let food = "indian"
+let puppyUrl = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + food + "&apiKey=16c525231b8e44dab6169ec9d64da6e5"
+
+
+
+$.ajax({
+    url: puppyUrl,
+    method: "GET",
+    
+}).then(function(response){
+        console.log(response);
+
+
+})     
