5c45b140851b661588c556dbfa36a7b43b4eef59
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-31 21:18:05 -0500

    added new components

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 //toggle switch between restaurant and recipe
 //check box for cuisine parameter, possible an array
 //click func for search
+
+// event listener for dropdown button
+//document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.dropdown-trigger');
+   // var instances = M.Dropdown.init(elems, options);
+  // });
